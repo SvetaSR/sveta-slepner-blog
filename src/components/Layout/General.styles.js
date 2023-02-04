8f28e352355b53a50693b2a1579f1b2a74c57757
Sveta Slepner <yoleis@gmail.com>
2023-02-05 00:25:20 +0200

    Delete src directory

--- src/components/Layout/General.styles.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const PageTitle = styled.h1`
-    font-size: 3rem;
-    margin: 0 0 2rem 0;
-    font-family: ${({ theme }) => theme.articleTitleFont}
-`;
